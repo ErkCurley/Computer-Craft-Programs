ebcf7c720efe259d99b533c135a6c6cc6814a669
Eric Curley <ErkCurley@users.noreply.github.com>
2020-10-10 14:18:20 -0400

    Update blueprint.lua

--- blueprint.lua
+++ blueprint.lua
@@ -8,21 +8,17 @@ blueprint = {}
 
 for j=1, length do
     blueprint[j] = {}
-    print(j)
     
     turtle.forward()
     turtle.turnRight()
 
     for i=1, width do
-        print(i)
-        print("-------")
         success, block = turtle.inspectDown()
         if success then
             blueprint[j][i] = block
         else
             blueprint[j][i] = {}        
         end
-          
         turtle.forward()
     end
     
